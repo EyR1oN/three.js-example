bb6d4070c41d0f78d22f42513dcfbfba9f5cab71
EyR1oN <o.govdish@gmail.com>
2025-05-12 19:59:11 +0300

    multipage

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,26 @@
 // vite.config.js
-import basicSsl from '@vitejs/plugin-basic-ssl'
+import basicSsl from "@vitejs/plugin-basic-ssl";
 
 export default {
   plugins: [
     basicSsl({
       /** name of certification */
-      name: 'test',
+      name: "test",
       /** custom trust domains */
-      domains: ['*.custom.com'],
+      domains: ["*.custom.com"],
       /** custom certification directory */
-      certDir: '/Users/.../.devServer/cert',
+      certDir: "/Users/.../.devServer/cert",
     }),
   ],
-}
+  build: {
+    rollupOptions: {
+      input: {
+        menu: resolve(__dirname, "index.html"),
+        task1: resolve(__dirname, "index1.html"),
+        task2: resolve(__dirname, "index2.html"),
+        task3: resolve(__dirname, "index3.html"),
+        task4: resolve(__dirname, "index4.html"),
+      },
+    },
+  },
+};
